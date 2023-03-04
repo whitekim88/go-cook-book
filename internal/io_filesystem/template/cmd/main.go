eae015dd4c6a28de2a3001c33f2af69b16fe05af
현성 김 <hs-kim@mantech.co.kr>
2023-03-04 16:29:07 +0900

    text/template, html/template study

---
+++ internal/io_filesystem/template/cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/whitekim88/go-cook-book/internal/io_filesystem/template"
+)
+
+func main() {
+	if err := template.RunTemplate(); err != nil {
+		panic(err)
+	}
+	if err := template.InitTemplate(); err != nil {
+		panic(err)
+	}
+	if err := template.HTMLDifferences(); err != nil {
+		panic(err)
+	}
+
+}
